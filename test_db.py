54835fa0416168e90374b470fe4e1dd8624b0bd2
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-11-24 20:01:35 +0000

    tests expanded

--- test_db.py
+++ test_db.py
@@ -46,3 +46,20 @@ class TestMusicDatabase(unittest.TestCase):
         Delete the database
         """
         os.remove("mydatabase.db")
+
+    def test_updating_artist(self):
+        """
+        Tests that we can successfully update an artist's name
+        """
+        simple_db.update_artist('Red', 'Redder')
+        actual = simple_db.select_all_albums('Redder')
+        expected = [('Until We Have Faces', 'Redder',
+                     '2/1/2011', 'Essential Records', 'CD')]
+        self.assertListEqual(expected, actual)
+
+    def test_artist_does_not_exist(self):
+        """
+        Test that an artist does not exist
+        """
+        result = simple_db.select_all_albums('Redder')
+        self.assertFalse(result)
